ef3bd209349aefae3284899e026d557da312cf47
cgallagher9 <geckosapien@hotmail.com>
2014-02-20 13:29:52 -0600

    Uploading Source Code File

---
+++ Assignment2.cpp
@@ -0,0 +1,19 @@
+﻿//Test file for assignment 2 in COMP 312.
+//Written by Connor Gallagher
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+string wait;
+
+cout<<"Hello, Dr. Butcher. This is my test file for assignment two. \nIt is written in C++ and hopefully fulfills your requirements for this assignment." <<endl;
+cout<<"Thanks!"<<endl;
+
+
+cin >> wait;
+return 0;
+}
