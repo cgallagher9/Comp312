458eb4b5613848f2c73a848467770d46107a927e
cgallagher9 <geckosapien@hotmail.com>
2014-04-04 16:22:08 -0500

    Upload of Project

---
+++ Project_Pilgrim/main.cpp
@@ -0,0 +1,16 @@
+/* 
+ * File:   main.cpp
+ * Author: Connor
+ *
+ * Created on April 4, 2014, 4:18 PM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout<<"Something about stuff."<<endl;
+    return 0;
+}
+
